949449ced49c5fcf6b5ff21dd5d1c1c940cf2171
José Luis Puentes <jose@Garfield-PC>
2019-03-07 23:02:07 +0100

    Acabat el Service locator i el UserDAO. TODO: TFFactory

---
+++ src/ioc/dam/m13/tutor_finder/server/TFFactory.java
@@ -0,0 +1,11 @@
+package ioc.dam.m13.tutor_finder.server;
+
+/**
+ *
+ * @author José Luis Puentes Jiménez <jlpuentes74@gmail.com>
+ */
+public class TFFactory {
+    
+    //TODO: Crear getInstance(String className)
+
+}
