232ca6ceb46cc6d5da1287ae8917e7f14f1c8a0e
José Luis Puentes <jose@Garfield-PC>
2019-03-08 08:45:24 +0100

    TFFactory acabada

--- src/ioc/dam/m13/tutor_finder/server/TFFactory.java
+++ src/ioc/dam/m13/tutor_finder/server/TFFactory.java
@@ -1,11 +1,30 @@
 package ioc.dam.m13.tutor_finder.server;
 
+import java.util.ResourceBundle;
+
 /**
  *
  * @author José Luis Puentes Jiménez <jlpuentes74@gmail.com>
  */
 public class TFFactory {
     
-    //TODO: Crear getInstance(String className)
+    public static Object getInstance(String objName) {
+        try {
+            //Llegim el factory.properties
+            ResourceBundle rb = ResourceBundle.getBundle("factory");
+            //Agafem la classe
+            String className = rb.getString(objName);
+            //Retornem una instancia de la classe que hem agafat
+            Object ret = Class.forName(className).newInstance();
+            
+            return ret;
+            
+        } catch (Exception e) {
+            
+            e.printStackTrace();
+            throw new RuntimeException(e);
+            
+        }
+    }
 
 }
